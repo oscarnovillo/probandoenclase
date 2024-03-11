e70ca57614471b185f0c5d11936f3582b4573c0f
cvcvrril <imartinezrodriguez@educa.madrid.org>
2024-03-11 09:30:46 +0100

    FIUUUUUUM

--- common/common.java
+++ common/common.java
@@ -19,3 +19,6 @@ carlos
 david
 
 rey
+
+
+eoooooooooooooooooooooooooooooooooooooooooo
--- data/Ines.java
+++ data/Ines.java
@@ -1,4 +1,11 @@
 package data;
 
 public class Ines {
+
+    public static void main(String[] args) {
+
+        System.out.println("EOOOOOOOOOOO");
+
+    }
+
 }
